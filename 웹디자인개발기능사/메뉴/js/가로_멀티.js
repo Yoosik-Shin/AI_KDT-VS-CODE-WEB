da0596f6d284a276313a31097b2e20c59a7013e2
Yoosik-Shin <snorlax234678@gmail.com>
2025-04-21 18:09:57 +0900

    Day 30 (WEB 11)

---
+++ 웹디자인개발기능사/메뉴/js/가로_멀티.js
@@ -0,0 +1,22 @@
+// 문서 준비 이벤트
+document.addEventListener("DOMContentLoaded", () => {
+  const mainMenuList = document.querySelectorAll('.main-menu > .menu-item')
+
+  mainMenuList.forEach(function(menu) {
+    // 해당 메인 메뉴의 서브 메뉴
+    const subMenuList = document.querySelectorAll('.submenu')
+
+    // 마우스 올렸을 때
+    menu.addEventListener("mouseover", () => {
+      subMenuList.forEach((submenu) => {
+        submenu.classList.add('active')
+      })
+    })
+    // 마우스 벗어날 때
+    menu.addEventListener("mouseout", () => {
+      subMenuList.forEach((submenu) => {
+        submenu.classList.remove('active')
+      });
+    })
+  })
+})
